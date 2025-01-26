67952c3df4e242c05a548a5c66f3d6d7b6c1c9a1
Phill <philldamaskin@gmail.com>
2025-01-26 14:26:42 -0500

    add docs

--- source/Extensions/StackExtensions.cs
+++ source/Extensions/StackExtensions.cs
@@ -5,9 +5,27 @@ namespace Collections
 {
     public static class StackExtensions
     {
+        /// <summary>
+        /// Checks if the stack contains <paramref name="value"/>.
+        /// </summary>
         public static bool Contains<T>(this Stack<T> stack, T value) where T : unmanaged, IEquatable<T>
         {
             return stack.AsSpan().Contains(value);
+        }
+
+        /// <summary>
+        /// Tries to push <paramref name="value"/> to the stack if it's not contained.
+        /// </summary>
+        /// <returns><c>true</c> if it was pushed.</returns>
+        public static bool TryPush<T>(this Stack<T> stack, T value) where T : unmanaged, IEquatable<T>
+        {
+            bool contains = stack.Contains(value);
+            if (!contains)
+            {
+                stack.Push(value);
+            }
+
+            return !contains;
         }
     }
 }
